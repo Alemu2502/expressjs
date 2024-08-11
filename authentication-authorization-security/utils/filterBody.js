e483ab6b09e58ab145d06af58672003858fa97f5
Alemu2502 <fre6819@gmail.com>
2024-08-11 21:57:42 +0300

    filter body

---
+++ authentication-authorization-security/utils/filterBody.js
@@ -0,0 +1,11 @@
+//  Filtering req.body to allow or not allowed to update
+
+module.exports = (body, allowedFields) => {
+  const newBody = {};
+
+  Object.keys(body).forEach((key) => {
+    if (allowedFields.includes(key)) newBody[key] = body[key];
+  });
+
+  return newBody;
+};
